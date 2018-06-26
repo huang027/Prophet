853c24e44afd8d50ec58157ec1c4ded279a36531
huang027 <38780704+huang027@users.noreply.github.com>
2018-06-26 16:31:54 +0800

    Add files via upload

---
+++ Prophet.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from fbprophet import Prophet
+import matplotlib.pyplot as plt
+from sklearn import metrics
+df=pd.read_excel('C:\\Users\\guojia\\Desktop\\data.xlsx')
+df['y']=df['y'].astype(float)
+m=Prophet(changepoint_prior_scale=0.15)
+m.fit(df)
+future=m.make_future_dataframe(periods=365)
+future.tail()
+forecast=m.predict(future)
+print(forecast[['ds','yhat','yhat_lower','yhat_upper']])
+m.plot(forecast)
+x1=forecast['ds']
+y1=forecast['yhat']
+plt.plot(x1,y1)
+plt.show()
+
+
+
